8ba24246c53c003b6afdaefd4360480381a9431e
Ben Wickerson <ben.wickerson@gmail.com>
2013-07-20 07:01:41 +0100

    Team changes

--- game.rb
+++ game.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__) + '/lib/control'
 system('clear')
 system('cls')
 
-Control.set_details(true)
+Control.set_details(false)
 trap("SIGINT") {puts " You decided to exit the game - silly you!"; exit}
 Control.go
